f0bf0103de89c9969a9b6846842dfad40c931147
Zion <zionholtsc@gmail.com>
2024-02-08 10:09:30 -0500

    Added multiple files to Array directory

---
+++ Arrays/multidimensional_arrays.cc
@@ -0,0 +1,16 @@
+//Copyright 2024 ZHOLT
+#include<iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+int main() {
+  //Looping through an array without specifying array size.
+  int more_integers[5] = {20, 40, 60, 80, 100};
+  for (int i : more_integers) {
+	  cout << i << endl;
+  }
+  //Multidimensional Arrays
+
+}

a2ad88b746a365dc063067c54c58f61370e612a7
Zion <zionholtsc@gmail.com>
2024-02-16 13:54:37 -0500

    Multiple Files Added

---
+++ Arrays/practice_programs/February16/array.cc
@@ -0,0 +1,10 @@
+//Copyright 2024 ZHOLT
+#include<iostream>
+#include"arrayfunctions.h"
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main() {
+  cout << "This is a sentence" << endl;
+}
---
+++ Arrays/practice_programs/February16/arrayfunctions.cc
@@ -0,0 +1,14 @@
+//Copyright 2024 ZHOLT
+
+#include<iostream>
+#include"arrayfunctions.h"
+using std::cout;
+using std::cin;
+using std::endl;
+
+void printFunction(int numbers[5], int size = 5) {
+  for(int i = 0; i < 5; i++) {
+    cout << numbers[i] << endl;
+  }
+}
+

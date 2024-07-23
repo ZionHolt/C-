ee71d96b2cb09aa3f3f54fb70139da3bd91120e0
Zion <zionholtsc@gmail.com>
2024-07-23 19:28:39 -0400

    Modify Program.cs
    - Added Console.WriteLine message.
    - Added Console.Write message.

--- Path1/Module1/HelloWorld/Program.cs
+++ Path1/Module1/HelloWorld/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Good morning world and all who inhabit it.");
+﻿Console.WriteLine("Good morning world and all who inhabit it.");
+Console.Write("Second line of code.");
+Console.Write(" This is appended to the previous line in the source code.");
+//Console - Class, WriteLine() - Method. Write() - Another method within the Console class.
